3d833de7ac849316028d9ebcbd46d14aa7be960e
sudip-mukherjee <sudip.mukherjee@tarento.com>
2019-01-24 04:12:11 +0530

    #Devcon-0000 feat: modified child details page

--- src/pages/child-details/child-details.ts
+++ src/pages/child-details/child-details.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { isArray } from 'ionic-angular/umd/util/util';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-child-details',
@@ -8,21 +9,34 @@ import { isArray } from 'ionic-angular/umd/util/util';
 })
 export class ChildDetailsPage {
   childrenDetail = [];
+  childrentInfoList = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) {
     console.log();
     this.childrenDetail = this.navParams.get("children");
     if (!Array.isArray(this.navParams.get("children"))) {
       this.childrenDetail = [this.navParams.get("children")];
     }
-  }
 
-  ionViewDidLoad() {
-
+    this.childrenDetail.forEach((element)=>{
+      this.getChildDetails(this.getChildName(element));
+    });
+    
   }
 
   getChildName(path) {
-    return  path.substring(path.lastIndexOf('/')+1);
+    return path.substring(path.lastIndexOf('/')+1);
+   
+  }
+
+  getChildDetails(studentId){
+    this.httpClient.get("https://dev.ekstep.in/api/devcon/v3/profile/read/" + studentId)
+      .subscribe((data: any) => {
+        console.log(data.result.Student);
+        this.childrentInfoList.push(data.result.Student);
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
